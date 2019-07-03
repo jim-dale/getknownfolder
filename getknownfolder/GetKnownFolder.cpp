42a1dc87f6f8fa6e1132c041d1093d563c902cc9
Jim Dale <26042891+jim-dale@users.noreply.github.com>
2019-07-03 14:08:00 +0100

    Changed WindowsTargetPlatformVersion to more generic 10.0 Changed C++ PlatformToolset to v142 Tweaked help text

--- getknownfolder/GetKnownFolder.cpp
+++ getknownfolder/GetKnownFolder.cpp
@@ -27,7 +27,7 @@ void ShowHelp()
 
     wprintf(L"  -?  Display this help information.\n");
     wprintf(L"  -v  Display version information.\n");
-    wprintf(L"  -a  [Default] Searches for the name by display name. If the name does not match a display name searches by canonical name.\n");
+    wprintf(L"  -a  [Default] Searches for the name by display name. If the name does not match a display name, searches by canonical name.\n");
     wprintf(L"  -c  Searches for the name by canonical name.\n");
     wprintf(L"  -d  Searches for the name by display name.\n\n");
     wprintf(L"If no parameters are supplied a list of known folders will be displayed in the format:-\n");
